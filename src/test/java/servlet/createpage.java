d37b15415144bc04ea2dbebfb5eb4b752960c823
Mayur <mayur.manyar222@gmail.com>
2020-09-17 20:21:49 +0530

    updated IP

--- src/test/java/servlet/createpage.java
+++ src/test/java/servlet/createpage.java
@@ -17,7 +17,7 @@ public class createpage extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://35.243.157.113:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://35.243.185.62:5432/postgres";
 			String username="postgres";
 			String password="password";
 		
