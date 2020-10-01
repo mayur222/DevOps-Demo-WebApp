22d616b67172cfa3b74e25912375fcd74aa6b8b4
Mayur <mayur.manyar222@gmail.com>
2020-10-01 21:19:29 +0530

    updated IP

--- src/test/java/servlet/viewticket.java
+++ src/test/java/servlet/viewticket.java
@@ -16,7 +16,7 @@ public class viewticket extends HttpServlet {
 		try
 		{
 			Class.forName("org.postgresql.Driver");
-			String jdbcUrl="jdbc:postgresql://35.243.185.62:5432/postgres";
+			String jdbcUrl="jdbc:postgresql://34.74.178.36:5432/postgres";
 			String username="postgres";
 			String password="password";
 						
